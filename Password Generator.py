f93387c4e217156b874e9a4b5a0cbd9d2331781f
Shroojan2076 <dhok.shroojan@gmail.com>
2025-12-16 01:25:21 +0530

    Refactor password generator with improved security
    Updated password generation logic to use secrets and string modules. Added validation for password length inputs.

--- Password Generator.py
+++ Password Generator.py
@@ -1,8 +1,9 @@
-import random as rd
+import secrets as sc
+import string as s
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+letters = s.ascii_letters
+numbers = s.digits
+symbols = s.punctuation
 
 print('Welcome to Password Generator!')
 
@@ -11,18 +12,25 @@ try:
     n_numbers = int(input('Enter the number of numbers you want in your password:\n'))
     n_symbols = int(input('Enter the number of symbols you want in your password:\n'))
 
-    password = ''
-    for s in range(n_letters):
-        password += rd.choice(letters)
-    for s in range(n_numbers):
-        password += rd.choice(numbers)
-    for s in range(n_symbols):
-        password += rd.choice(symbols)
-    l = list(password)
-    rd.shuffle(l)
-    password = "".join(l)
-    print("Here's your new Password:")
-    print(password)
+    if n_letters <= 0 and n_numbers <= 0 and n_symbols <= 0:
+        print("Provided password length is too small.")
+       
+    elif n_letters >= 100 or n_numbers >= 100 or n_symbols >= 100:
+        print("Provided password length is too large.")
 
+    else:   
+        password = []
+
+        for let in range(n_letters):
+            password.append(sc.choice(letters))
+        for num in range(n_numbers):
+            password.append(sc.choice(numbers))
+        for sym in range(n_symbols):
+            password.append(sc.choice(symbols))
+
+        sc.SystemRandom.shuffle(password)
+        print("Here's your new Password:")
+        print("".join(password))
+    
 except ValueError:
     print('Please enter a valid value. Program stopped.')
